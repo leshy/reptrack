476b518b0f8837920b7a3f5dd835590206377e4c
lesh <lesh@sysphere.org>
2025-02-21 21:55:43 +0800

    fixing relative urls

--- clientside/clientside.ts
+++ clientside/clientside.ts
@@ -163,7 +163,7 @@ class PoseCenter extends EventEmitter<PoseEvent> {
 }
 
 async function init() {
-    const video = new Video("./sample.mp4")
+    const video = new Video("../sample.mp4")
     const poseEstimator = new PoseEstimator(video)
     const poseCenter = new PoseCenter(poseEstimator)
     const svg = wm.createSvgWindow()
--- readme.md
+++ readme.md
@@ -1,4 +1,7 @@
 ## Reptrack
+
+deployed at [https://leshy.github.io/reptrack](https://leshy.github.io/reptrack)
+
 ![screenshot](./misc/screenshot.png)
 
 
