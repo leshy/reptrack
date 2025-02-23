c508131556917445dd57fd1ca8abf3a21469c52f
lesh <lesh@sysphere.org>
2025-02-23 19:22:42 +0800

    euclidian filter pose weighted avg

--- clientside/binary/transforms.ts
+++ clientside/binary/transforms.ts
@@ -153,3 +153,77 @@ export class EuclidianFilter extends EventEmitter<BinaryPoseEvent> {
         }
     }
 }
+
+export function weightedAveragePoses(poses: Pose[]): Pose {
+    const avgPose = new Pose()
+    if (poses.length === 0) return avgPose
+
+    // Compute weighted average for timestamp using pose score as weight.
+    let totalPoseScore = 0
+    let sumTimestamp = 0
+    for (const pose of poses) {
+        totalPoseScore += pose.score
+        sumTimestamp += pose.timestamp * pose.score
+    }
+    avgPose.timestamp = totalPoseScore ? sumTimestamp / totalPoseScore : 0
+
+    // Combine overall score (using square-root of average squared scores).
+    const sumScoreSq = poses.reduce((sum, pose) => sum + pose.score ** 2, 0)
+    avgPose.score = Math.min(1, Math.sqrt(sumScoreSq / poses.length))
+
+    // For each keypoint, compute weighted average positions.
+    for (let i = 0; i < Pose.keypointCount; i++) {
+        let totalKeypointScore = 0
+        let sumX = 0
+        let sumY = 0
+        let sumKeypointScoreSq = 0
+
+        for (const pose of poses) {
+            const [x, y, s] = pose.getKeypoint(i)
+            totalKeypointScore += s
+            sumX += x * s
+            sumY += y * s
+            sumKeypointScoreSq += s * s
+        }
+        const avgX = totalKeypointScore ? sumX / totalKeypointScore : 0
+        const avgY = totalKeypointScore ? sumY / totalKeypointScore : 0
+        const avgS = totalKeypointScore
+            ? Math.min(1, Math.sqrt(sumKeypointScoreSq / poses.length))
+            : 0
+
+        avgPose.setKeypoint(i, [avgX, avgY, avgS])
+    }
+
+    return avgPose
+}
+
+export class ConfidentEuclidianFilter extends EventEmitter<BinaryPoseEvent> {
+    private lastPose: Pose | null = null
+    private previously: Pose[] = []
+
+    constructor(
+        poseEmitter: BinaryPoseEmitter,
+        private threshold: number = 5,
+    ) {
+        super()
+        this.threshold = threshold
+        poseEmitter.on("pose", this.process)
+    }
+
+    process = (pose: Pose) => {
+        if (!this.lastPose) {
+            this.lastPose = pose
+            return
+        }
+
+        const distance = this.lastPose.distance(pose)
+
+        if (distance > this.threshold) {
+            this.lastPose = pose
+            this.emit("pose", weightedAveragePoses(this.previously))
+            this.previously = []
+        } else {
+            this.previously.push(pose)
+        }
+    }
+}
